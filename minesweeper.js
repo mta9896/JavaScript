cde5c5c2dca7af478c1732a83bc13e22793bf3e1
mta9896 <mta9896@gmail.com>
2017-08-06 10:49:15 +0430

    Added win and lose handling

--- minesweeper.js
+++ minesweeper.js
@@ -2,13 +2,33 @@ var screenArray = [];
 var isLost = false;
 window.onload = function () {
         genrateGrid();
+        initialize();
+}
 
-        var outerDiv = document.createElement('div');
+function genrateGrid() {
+    for (var i = 0 ; i < 10 ; i++) {
+    screenArray[i] = [];
+      for (var j = 0; j < 10; j++) {
+        screenArray[i][j] = 0;
+      }
+  }
+
+    for(var i = 0; i<10; i++){
+      var width = (Math.random()* 10 | 0);
+      var height = (Math.random()* 10 | 0);
+      screenArray[width][height] = 1;
+    }
+    console.table(screenArray);
+    return screenArray
+}
+
+function initialize() {
+    var outerDiv = document.createElement('div');
         outerDiv.id = 'alert-modal';
         outerDiv.className = 'modal';
         var innerDiv = document.createElement('div');
         innerDiv.className = 'modal-content'
-        var okButton = document.createElement('Button');
+        let okButton = document.createElement('Button');
         var t = document.createTextNode("OK");
         okButton.appendChild(t);
         var nameInput = document.createElement("INPUT");
@@ -62,23 +82,7 @@ window.onload = function () {
         windowDiv.appendChild(content);
         document.body.appendChild(windowDiv);
         document.body.appendChild(outerDiv);
-}
 
-function genrateGrid() {
-    for (var i = 0 ; i < 10 ; i++) {
-    screenArray[i] = [];
-      for (var j = 0; j < 10; j++) {
-        screenArray[i][j] = 0;
-      }
-  }
-
-    for(var i = 0; i<10; i++){
-      var width = (Math.random()* 10 | 0);
-      var height = (Math.random()* 10 | 0);
-      screenArray[width][height] = 1;
-    }
-    console.table(screenArray);
-    return screenArray
 }
 
 function initScreen(content) {
@@ -91,25 +95,37 @@ function initScreen(content) {
             let btn= document.createElement('Button');
                 btn.classList.add("col");
             btn.id = i;
-            if(screenArray[i][j] === 1)
-                btn.style.backgroundColor="red";
+            // if(screenArray[i][j] === 1)
+            //     btn.style.backgroundColor="darkred";
             btn.onclick=function () {
                 btn.classList.add("col-clicked");
-                let bombs = clicked(i,j);
+                let bombs = findBombCount(i,j);
                 console.log("bombs "+bombs);
-                // let bombs = 0;
-                if(!isLost ) {
-                    if(bombs !== 0)
+                if(!lose(i,j) ) {
+                    if(bombs !== 0) {
                         btn.innerHTML = bombs;
+                    }
                     if (bombs === 0){
                         screenArray[i][j] = 2;
                         revealNeighbours(i,j);
-                        // console.table(screenArray);
                     }
                     else
                         showBombCount(i,j);
                 }
+                else{ //when player loses
+
+
+                }
+                console.table(screenArray);
+                win();
             }
+            btn.onmousedown = function(event) {
+                if (event.which == 3) {
+                    setFlag(i,j);
+                }
+            }
+
+
             rowDiv.appendChild(btn);
         }
         content.appendChild(rowDiv);
@@ -117,24 +133,6 @@ function initScreen(content) {
 }
 
 
-function clicked(width, height) {
-        if(screenArray[width][height] === 1){
-            isLost = true;
-            for(let i=0; i<10; i++){
-                for(let j=0;j<10; j++){
-                    if(screenArray[i][j]=== 1){
-                        document.getElementsByClassName("row")[j].getElementsByClassName("col")[i].style.backgroundColor = "red";
-                        // let ddd = document.getElementById(i);
-                        // console.log(ddd.id);
-                        // console.log(i + " , " +j);
-                    }
-
-                }
-            }
-             // alert("you lost");
-        }
-        return findBombCount(width,height);
-}
 
 function revealNeighbours(height,width){ // a recursive function
 
@@ -142,66 +140,74 @@ function revealNeighbours(height,width){ // a recursive function
         if ((width - 1 > 0) && (height - 1 > 0) && (screenArray[height-1][width-1] !== 2) && findBombCount(height - 1, width - 1) === 0) {
             screenArray[height-1][width-1] = 2;
             revealNeighbours(height - 1, width - 1);
-            document.getElementsByClassName("row")[width-1].getElementsByClassName("col")[height-1].classList.add("col-clicked");
+            if(document.getElementsByClassName("row")[width-1].getElementsByClassName("col")[height-1].innerHTML !== '⛿')
+                document.getElementsByClassName("row")[width-1].getElementsByClassName("col")[height-1].classList.add("col-clicked");
 
         }
-        if ((width - 1 > 0) && (height - 1 > 0) && (screenArray[height-1][width-1] !== 2) && findBombCount(height - 1, width - 1) !== 0)
+        if ((width - 1 > 0) && (height - 1 > 0) && (screenArray[height-1][width-1] !== 2) && findBombCount(height - 1, width - 1) !== 0 && (document.getElementsByClassName("row")[width-1].getElementsByClassName("col")[height-1].innerHTML !== '⛿'))
             showBombCount(height-1 , width-1);
 
         if ((height - 1 > 0) && (screenArray[height-1][width] !== 2) && findBombCount(height - 1, width) === 0) {
             screenArray[height - 1][width] = 2;
             revealNeighbours(height - 1, width);
-            document.getElementsByClassName("row")[width].getElementsByClassName("col")[height-1].classList.add("col-clicked");
+            if(document.getElementsByClassName("row")[width].getElementsByClassName("col")[height-1].innerHTML !== '⛿')
+                document.getElementsByClassName("row")[width].getElementsByClassName("col")[height-1].classList.add("col-clicked");
         }
-        if ((height - 1 > 0) && (screenArray[height-1][width] !== 2) && findBombCount(height - 1, width) !== 0)
+        if ((height - 1 > 0) && (screenArray[height-1][width] !== 2) && findBombCount(height - 1, width) !== 0 && (document.getElementsByClassName("row")[width].getElementsByClassName("col")[height-1].innerHTML !== '⛿'))
             showBombCount(height-1 , width);
 
         if ((height - 1 > 0) && (width + 1 < 10) && (screenArray[height-1][width+1] !== 2) && findBombCount(height - 1, width + 1) === 0) {
             screenArray[height - 1][width + 1] = 2;
             revealNeighbours(height - 1, width + 1);
-            document.getElementsByClassName("row")[width+1].getElementsByClassName("col")[height-1].classList.add("col-clicked");
+            if(document.getElementsByClassName("row")[width+1].getElementsByClassName("col")[height-1].innerHTML !== '⛿')
+                document.getElementsByClassName("row")[width+1].getElementsByClassName("col")[height-1].classList.add("col-clicked");
         }
-        if ((height - 1 > 0) && (width + 1 < 10) && (screenArray[height-1][width+1] !== 2) && findBombCount(height - 1, width + 1) !== 0)
+        if ((height - 1 > 0) && (width + 1 < 10) && (screenArray[height-1][width+1] !== 2) && findBombCount(height - 1, width + 1) !== 0 && (document.getElementsByClassName("row")[width+1].getElementsByClassName("col")[height-1].innerHTML !== '⛿'))
             showBombCount(height-1 , width+1);
 
         if ((width - 1 > 0) && (screenArray[height][width-1] !== 2) && findBombCount(height, width - 1) === 0) {
             screenArray[height][width - 1] = 2;
             revealNeighbours(height, width - 1);
-            document.getElementsByClassName("row")[width-1].getElementsByClassName("col")[height].classList.add("col-clicked");
+            if(document.getElementsByClassName("row")[width-1].getElementsByClassName("col")[height].innerHTML !== '⛿')
+                document.getElementsByClassName("row")[width-1].getElementsByClassName("col")[height].classList.add("col-clicked");
         }
-        if ((width - 1 > 0) && (screenArray[height][width-1] !== 2) && findBombCount(height, width - 1) !== 0)
+        if ((width - 1 > 0) && (screenArray[height][width-1] !== 2) && findBombCount(height, width - 1) !== 0 && (document.getElementsByClassName("row")[width-1].getElementsByClassName("col")[height].innerHTML !== '⛿'))
             showBombCount(height , width-1);
 
         if ((width + 1 < 10) && (screenArray[height][width+1] !== 2) && findBombCount(height, width + 1) === 0) {
             screenArray[height][width + 1] = 2;
             revealNeighbours(height, width + 1);
-            document.getElementsByClassName("row")[width+1].getElementsByClassName("col")[height].classList.add("col-clicked");
+            if(document.getElementsByClassName("row")[width+1].getElementsByClassName("col")[height].innerHTML !== '⛿')
+                document.getElementsByClassName("row")[width+1].getElementsByClassName("col")[height].classList.add("col-clicked");
         }
-        if ((width + 1 < 10) && (screenArray[height][width+1] !== 2) && findBombCount(height, width + 1) !== 0)
+        if ((width + 1 < 10) && (screenArray[height][width+1] !== 2) && findBombCount(height, width + 1) !== 0 && (document.getElementsByClassName("row")[width+1].getElementsByClassName("col")[height].innerHTML !== '⛿'))
             showBombCount(height , width+1);
 
         if ((height + 1 < 10) && (width - 1 > 0) && (screenArray[height+1][width-1] !== 2) && findBombCount(height + 1, width - 1) === 0) {
             screenArray[height + 1][width - 1] = 2;
             revealNeighbours(height + 1, width - 1);
-            document.getElementsByClassName("row")[width-1].getElementsByClassName("col")[height+1].classList.add("col-clicked");
+            if(document.getElementsByClassName("row")[width-1].getElementsByClassName("col")[height+1].innerHTML !== '⛿')
+                document.getElementsByClassName("row")[width-1].getElementsByClassName("col")[height+1].classList.add("col-clicked");
         }
-        if ((height + 1 < 10) && (width - 1 > 0) && (screenArray[height+1][width-1] !== 2) && findBombCount(height + 1, width - 1) !== 0)
+        if ((height + 1 < 10) && (width - 1 > 0) && (screenArray[height+1][width-1] !== 2) && findBombCount(height + 1, width - 1) !== 0 && (document.getElementsByClassName("row")[width-1].getElementsByClassName("col")[height+1].innerHTML !== '⛿'))
             showBombCount(height+1 , width-1);
 
         if ((height + 1 < 10) && (screenArray[height+1][width] !== 2) && findBombCount(height + 1, width) === 0) {
             screenArray[height + 1][width] = 2;
             revealNeighbours(height + 1, width);
-            document.getElementsByClassName("row")[width].getElementsByClassName("col")[height+1].classList.add("col-clicked");
+            if(document.getElementsByClassName("row")[width].getElementsByClassName("col")[height+1].innerHTML !== '⛿')
+                document.getElementsByClassName("row")[width].getElementsByClassName("col")[height+1].classList.add("col-clicked");
         }
-        if ((height + 1 < 10) && (screenArray[height+1][width] !== 2) && findBombCount(height + 1, width) !== 0)
+        if ((height + 1 < 10) && (screenArray[height+1][width] !== 2) && findBombCount(height + 1, width) !== 0 && (document.getElementsByClassName("row")[width].getElementsByClassName("col")[height+1].innerHTML !== '⛿'))
             showBombCount(height+1 , width);
 
         if ((height + 1 < 10) && (width + 1 < 10) && (screenArray[height+1][width+1] !== 2) && findBombCount(height + 1, width + 1) === 0) {
             screenArray[height + 1][width + 1] = 2;
             revealNeighbours(height + 1, width + 1);
-            document.getElementsByClassName("row")[width+1].getElementsByClassName("col")[height+1].classList.add("col-clicked");
+            if(document.getElementsByClassName("row")[width+1].getElementsByClassName("col")[height+1].innerHTML !== '⛿')
+                document.getElementsByClassName("row")[width+1].getElementsByClassName("col")[height+1].classList.add("col-clicked");
         }
-        if ((height + 1 < 10) && (width + 1 < 10) && (screenArray[height+1][width+1] !== 2) && findBombCount(height + 1, width + 1) !== 0)
+        if ((height + 1 < 10) && (width + 1 < 10) && (screenArray[height+1][width+1] !== 2) && findBombCount(height + 1, width + 1) !== 0 && (document.getElementsByClassName("row")[width+1].getElementsByClassName("col")[height+1].innerHTML !== '⛿'))
             showBombCount(height+1 , width+1);
 
 }
@@ -224,7 +230,6 @@ function findBombCount(width, height){
         bombCount++;
     if((height-1>-1) && screenArray[width][height-1] === 1)
         bombCount++;
-    // console.log("number of bombs around " + width+ " , "+ height+ " : " + bombCount);
     return bombCount;
 }
 
@@ -272,4 +277,79 @@ function showBombCount(i, j) {
 
     document.getElementsByClassName("row")[j].getElementsByClassName("col")[i].style.color = color;
 
+}
+
+function setFlag(height,width) {
+    if(document.getElementsByClassName("row")[width].getElementsByClassName("col")[height].innerHTML == '⛿') {
+    }
+    else{
+        document.getElementsByClassName("row")[width].getElementsByClassName("col")[height].innerHTML = '⛿';
+    }
+
+}
+
+function win() {
+    let k, t;
+    let won = true;
+    console.log("win");
+    for(t=0; t<10; t++){
+        for(k=0; k<10; k++){
+            if( !(document.getElementsByClassName("row")[t].getElementsByClassName("col")[k].classList.contains("col-clicked")) && !(document.getElementsByClassName("row")[t].getElementsByClassName("col")[k].innerHTML =='⛿'))
+                won = false;
+        }
+    }
+    console.log("won = " +won);
+    if(won) {
+        var winAlert = document.createElement('div');
+        winAlert.className = 'win-lose-alert';
+        let text = document.createTextNode("You Won");
+        winAlert.appendChild(text);
+        let okButton = document.createElement('Button');
+        let okText = document.createTextNode("");
+        okButton.appendChild(okText);
+        okButton.className = 'ok-button';
+        winAlert.appendChild(okButton);
+        document.body.appendChild(winAlert);
+        okButton.onclick = function(){
+            document.body.removeChild(winAlert);
+            document.body.removeChild(document.getElementsByClassName('window')[0]);
+            genrateGrid();
+            initialize();
+        }
+    }
+}
+
+function lose(height,width) {
+    var isLost = false;
+    if(screenArray[height][width] === 1){
+            isLost = true;
+            for(let i=0; i<10; i++){
+                for(let j=0;j<10; j++){
+                    if(screenArray[i][j]=== 1){
+                        document.getElementsByClassName("row")[j].getElementsByClassName("col")[i].style.backgroundColor = "red";
+                    }
+
+                }
+            }
+        }
+        if(isLost){
+            var loseAlert = document.createElement('div');
+            loseAlert.className = 'win-lose-alert';
+            let text = document.createTextNode("You Lost");
+            loseAlert.appendChild(text);
+            let okButton = document.createElement('Button');
+            let okText = document.createTextNode("New Game");
+            okButton.appendChild(okText);
+            okButton.className = 'ok-button';
+            loseAlert.appendChild(okButton);
+            document.body.appendChild(loseAlert);
+            okButton.onclick = function(){
+                document.body.removeChild(loseAlert);
+                document.body.removeChild(document.getElementsByClassName('window')[0]);
+                genrateGrid();
+                initialize();
+            }
+        }
+        return isLost;
+    
 }
